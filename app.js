ff671e1286f748ae42b2a6efadda82dd5eecd399
njacquiot <nicojacq28@orange.fr>
2019-03-25 11:50:59 +0100

    nico

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port=process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
-	res.end('<h1>Hello World by Andrea</h1><p>How are things? Cool stuff this HEROKU! </p>');
+	res.end('<h1>Hello World by Nico</h1><p>How are things? Cool stuff this HEROKU! </p>');
 });
 
 server.listen(port,() => {
